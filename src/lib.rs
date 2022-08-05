c16bc26dcdb9fef275ffedff50e8da8ecc71edae
laralove <laralove143@icloud.com>
2022-08-06 00:17:32 +0300

    initialize crate

---
+++ src/lib.rs
@@ -0,0 +1,10 @@
+//! A sophisticated framework for the Discord API
+//!
+//! WIP
+
+#![warn(clippy::cargo, clippy::nursery, clippy::pedantic, clippy::restriction)]
+#![allow(
+    clippy::blanket_clippy_restriction_lints,
+    clippy::implicit_return,
+    clippy::pattern_type_mismatch
+)]
