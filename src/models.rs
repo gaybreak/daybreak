1f333b6096b310e7c9329ec507d95882dab343f8
laralove <laralove143@icloud.com>
2022-08-19 00:22:30 +0300

    Add Id struct

--- src/models.rs
+++ src/models.rs
@@ -8,3 +8,22 @@
     clippy::missing_docs_in_private_items
 )]
 
+#[doc = discord_url!("https://discord.com/developers/docs/reference#snowflakes")]
+pub struct Id(u64);
+
+impl Id {
+    /// The unix timestamp of the ID
+    #[doc = discord_url!(
+        "https://discord.com/developers/docs/reference
+        #snowflakes-snowflake-id-format-structure-left-to-right"
+    )]
+    pub fn timestamp(self) -> InternalResult<OffsetDateTime> {
+        Ok(OffsetDateTime::from_unix_timestamp(
+            ((self.0.checked_shr(22))
+                .ok()?
+                .checked_add(1_420_070_400_000))
+            .ok()?
+            .try_into()?,
+        )?)
+    }
+}
