d0945b79bf6b78fb0f3d8ceb4071d1ca1f2f2d48
laralove <laralove143@icloud.com>
2022-08-31 01:55:33 +0300

    Add method to make a request

--- src/context.rs
+++ src/context.rs
@@ -1,13 +1,27 @@
-use crate::http::{self, Http};
+use enumflags2::{BitFlag, BitFlags};
+
+use crate::{
+    http::{self, Http},
+    model::Permissions,
+};
 
 // /// Context methods about the audit log
 // pub mod audit_log;
+#[doc = discord_url!(
+    "https://discord.com/developers/docs/resources/auto-moderation\
+    #list-auto-moderation-rules-for-guild"
+)]
+pub mod auto_moderation;
 
 #[derive(Debug)]
 /// Brings all of the stateful structs together
 pub struct Context {
+    /// The bot's token, unlike `Config.token`, this has `Bot ` prepended to it
+    pub(crate) token: String,
+    /// The permissions the bot has, this will be removed at release
+    pub(crate) permissions: BitFlags<Permissions>,
     /// The HTTP client used in the crate
-    http: Http,
+    pub(crate) http: Http,
 }
 
 impl Context {
@@ -31,6 +45,8 @@ impl Context {
     #[allow(clippy::new_without_default)]
     pub fn new(config: &Config<'_>) -> Self {
         Self {
+            token: format!("Bot {}", config.token),
+            permissions: Permissions::all(),
             http: http::create(),
         }
     }
--- src/http.rs
+++ src/http.rs
@@ -1,8 +1,24 @@
-use hyper::{client::HttpConnector, Client};
+use enumflags2::BitFlags;
+use hyper::{client::HttpConnector, Body, Client, Method, Request, Response};
 use hyper_rustls::{HttpsConnector, HttpsConnectorBuilder};
+use thiserror::Error;
+
+use crate::{context::Context, model::Permissions};
 
 /// The client type used in this crate
 pub type Http = Client<HttpsConnector<HttpConnector>>;
+pub type HttpResult<T> = Result<T, HttpError>;
+
+#[derive(Error, Debug)]
+pub enum HttpError {
+    #[error("The bot doesn't have the required permissions to make this request: {0}")]
+    MissingPermissions(BitFlags<Permissions>),
+    #[error(
+        "Unexpected error, if this is an error with Daybreak, please open an issue at \
+        https://github.com/gaybreak/daybreak/issues/new: {0}"
+    )]
+    Other(#[from] anyhow::Error),
+}
 
 /// Creates the HTTP client
 pub fn create() -> Http {
@@ -12,4 +28,37 @@ pub fn create() -> Http {
         .enable_http2()
         .build();
     Client::builder().build(connector)
+}
+
+pub struct DiscordRequest<T> {
+    required_permissions: BitFlags<Permissions>,
+    method: Method,
+    endpoint: String,
+    returns: T,
+}
+
+impl Context {
+    async fn send_request<T>(&self, request: DiscordRequest<T>) -> HttpResult<Response<Body>> {
+        if !self.permissions.contains(request.required_permissions) {
+            return Err(HttpError::MissingPermissions(
+                !(self.permissions & request.required_permissions),
+            ));
+        }
+
+        self.http
+            .request(
+                Request::builder()
+                    .method(request.method)
+                    .uri(format!("https://discord.com/api/v10/{}", request.endpoint))
+                    .header(
+                        "User-Agent",
+                        "DiscordBot (https://github.com/gaybreak/daybreak 0.1)",
+                    )
+                    .header("Authorization", &self.token)
+                    .body(Body::empty())
+                    .map_err(|err| HttpError::Other(err.into()))?,
+            )
+            .await
+            .map_err(|err| HttpError::Other(err.into()))
+    }
 }
